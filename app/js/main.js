3821470338573be3523350421b70bfb685f1f625
Ejurov Konstantin <uchiha_ej@mail.ru>
2022-08-19 11:10:12 +0300

    end

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".arrow-top").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+});
